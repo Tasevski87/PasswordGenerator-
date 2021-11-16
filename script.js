e1cb722be3610587ab83e8eac74677b42ff4c1e5
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2021-11-16 14:59:47 -0500

    created generate password function thats promting the user for a lenght and error checking

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var number = '1234567890'
+var specialCharacers = '!@#$%^&*()_+<>?,./'
+
+
+function generatePassword() {
+  var password = ''
+
+  var pwdLength = window.prompt("Between 8 adn 128 how many characters would you like your password to contain?");
+
+  while (pwdLength < 8 || pwdLength > 128 || isNaN(pwdLength) === true) {
+    alert("Your password must be a number and it must be tween 8 and 128.")
+    pwdLength = window.prompt("Between 8 adn 128 how many characters would you like your password to contain?");
+  }
+
+
+
+  console.log(pwdLength);
+
+  return password
+}
 
 // Write password to the #password input
 function writePassword() {
