8d04db378e01bc91797b23939e40cd606ba6f502
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2021-11-17 21:19:35 -0500

    update on missing special characters and syntax

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ var generateBtn = document.querySelector("#generate");
 var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var number = '1234567890'
-var specialCharacers = '!@#$%^&*()_+<>?,./'
+var specialCharacers = '!@#$%^&*()_+<>?,./=+-;{|}[\]:~'
 
-// I created a funcion 
+// we created a funcion 
 function generatePassword() {
   var password = '';
   var allValues = '';
 
-// I created window task that ask you Between 8 and 128 how many characters would you like your password to contain?
+// we created window task that ask you Between 8 and 128 how many characters would you like your password to contain?
   var pwdLength = window.prompt("Between 8 and 128 how many characters would you like your password to contain?");
 
   //Over here we are checkin does your entered number is in our set range or is our number < 8  or  > than 128
   while (pwdLength < 8 || pwdLength > 128 || isNaN(pwdLength) === true) {
     alert("Your password must be a number and it must be tween 8 and 128.")
-    pwdLength = window.prompt("Between 8 adn 128 how many characters would you like your password to contain?");
+    pwdLength = window.prompt("Between 8 and 128 how many characters would you like your password to contain?");
   }
 
   //This window ask about options with your password like Do you want uppercase letters?, Do you want lowercase letters?, Do you want numbers?, Do you want special characters?
-  var isUpperCase = window.confirm("Do you want uppercase letters?")
-  var isLowerCase = window.confirm("Do you want lowercase letters?")
-  var hasNumbers = window.confirm("Do you want numbers?")
-  var hasSpecialCharacters = window.confirm("Do you want special characters?")
+  var isUpperCase = window.confirm("Do you want uppercase letters?");
+  var isLowerCase = window.confirm("Do you want lowercase letters?");
+  var hasNumbers = window.confirm("Do you want numbers?");
+  var hasSpecialCharacters = window.confirm("Do you want special characters?");
   
   // Over here we are checkin did you chose at least one type of caracter when the window asked you 
   while (!isUpperCase && !isLowerCase && !hasNumbers && !hasSpecialCharacters) {
@@ -37,24 +37,24 @@ function generatePassword() {
 
   //Over here we are checking which special caracter you chose when the window asked you
   if (isUpperCase) {
-    allValues += upperCase
+    allValues += upperCase;
   }
   if (isLowerCase) {
-    allValues += lowerCase
+    allValues += lowerCase;
   }
   if (hasNumbers) {
-    allValues += number
+    allValues += number;
   }
   if(hasSpecialCharacters) {
-    allValues += specialCharacers
+    allValues += specialCharacers;
   }
 
   // We created a for loop who is randomly generating password with the caracters that we chose when the window ask
   for (var i = 0; i < pwdLength; i++) {
-    password += allValues.charAt(Math.floor(Math.random() * allValues.length))
+    password += allValues.charAt(Math.floor(Math.random() * allValues.length));
   }
 
-  return password
+  return password;
 }
 
 // Write password to the #password input
